42eb18a9201170bac3bd0ae8b242874e39b7d5ea
Sébastien Boulet <sebastien.boulet@gmail.com>
2020-01-04 21:27:05 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod piece_evaluation;
 mod utils;
 
-use chess::{Board, BoardBuilder, ChessMove, Color, Game, MoveGen, Piece, Square, ALL_SQUARES};
-//use rand::seq::IteratorRandom;
 use crate::piece_evaluation::evaluate_piece;
+use chess::{Board, ChessMove, Game, MoveGen, Square, ALL_SQUARES};
 use std::cmp::Ordering::Equal;
-use wasm_bindgen::__rt::core::f32::MIN;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -31,7 +29,7 @@ pub struct RChess {
     game: Game,
 }
 
-static mut eval_count: isize = 0;
+static mut EVAL_COUNT: isize = 0;
 
 #[wasm_bindgen]
 impl RChess {
@@ -56,16 +54,16 @@ impl RChess {
 
     pub fn compute_move(&mut self) -> String {
         unsafe {
-            eval_count = 0;
+            EVAL_COUNT = 0;
         }
-        let mut move_gen = MoveGen::new_legal(&self.game.current_position());
-        //        let chess_move = move_gen.choose(&mut rand::thread_rng());
+        let depth = 4;
+        let move_gen = MoveGen::new_legal(&self.game.current_position());
         move_gen
             .map(|chess_move| {
                 let new_board = &self.game.current_position().make_move_new(chess_move);
                 (chess_move, {
                     let is_maximising_player = false;
-                    minimax(new_board, 3, -10000.0, 10000.0, is_maximising_player)
+                    minimax(new_board, depth - 1, -10000.0, 10000.0, is_maximising_player)
                 })
             })
             .max_by(|(_, estimation1), (_, estimation2)| {
@@ -75,7 +73,7 @@ impl RChess {
             .into_iter()
             .for_each(|chess_move| {
                 unsafe {
-                    console_log!("best move {} among {}", chess_move, eval_count);
+                    console_log!("best move {} among {}", chess_move, EVAL_COUNT);
                 }
                 &self.game.make_move(chess_move);
             });
@@ -85,68 +83,46 @@ impl RChess {
 }
 
 fn minimax(board: &Board, depth: u32, alpha: f32, beta: f32, is_maximising_player: bool) -> f32 {
+    unsafe {
+        EVAL_COUNT = EVAL_COUNT + 1;
+    }
     if depth == 0 {
         return -evaluate_board(board);
     }
-    let mut move_gen = MoveGen::new_legal(board);
-    let evaluation = if is_maximising_player {
-        let mut best_evaluation: f32 = -9999.0;
-        let mut alpha = alpha;
-        for chess_move in move_gen {
-            let new_board = board.make_move_new(chess_move);
-            best_evaluation = best_evaluation.max(minimax(
-                &new_board,
-                depth - 1,
-                alpha,
-                beta,
-                !is_maximising_player,
-            ));
-
-            alpha = alpha.max(best_evaluation);
-            if beta <= alpha {
-                break;
-            }
-        }
-        best_evaluation
+    let move_gen = MoveGen::new_legal(board);
+    let mut best_evaluation: f32 = if is_maximising_player {
+        -9999.0
     } else {
-        let mut best_evaluation: f32 = 9999.0;
-        let mut beta = beta;
-        for chess_move in move_gen {
-            let new_board = board.make_move_new(chess_move);
-            best_evaluation = best_evaluation.min(minimax(
-                &new_board,
-                depth - 1,
-                alpha,
-                beta,
-                !is_maximising_player,
-            ));
-            beta = beta.min(best_evaluation);
-            if beta <= alpha {
-                break;
-            }
-        }
-        best_evaluation
+        9999.0
     };
-    //    console_log!(
-    //        "{} - evaluation {}",
-    //        "  ".repeat((3 - depth) as usize),
-    //        evaluation
-    //    );
-    evaluation
+    let mut alpha = alpha;
+    let mut beta = beta;
+    for chess_move in move_gen {
+        let new_board = board.make_move_new(chess_move);
+        let evaluation = minimax(&new_board, depth - 1, alpha, beta, !is_maximising_player);
+        if is_maximising_player {
+            best_evaluation = best_evaluation.max(evaluation);
+            alpha = alpha.max(best_evaluation);
+        } else {
+            best_evaluation = best_evaluation.min(evaluation);
+            beta = beta.min(best_evaluation);
+        }
+        if beta <= alpha {
+            break;
+        }
+    }
+    best_evaluation
 }
 
 fn evaluate_board(board: &Board) -> f32 {
-    unsafe {
-        eval_count = eval_count + 1;
-    }
-    ALL_SQUARES.iter().fold(0.0, |acc, &square| {
-        acc + board
-            .color_on(square)
-            .and_then(|color| {
+    ALL_SQUARES
+        .iter()
+        .filter_map(|&square| {
+            board.color_on(square).and_then(|color| {
                 board
                     .piece_on(square)
                     .map(|piece| evaluate_piece(piece, color, square))
             })
-            .unwrap_or(0.0)
-    })
+        })
+        .sum()
 }
