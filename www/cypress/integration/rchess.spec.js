5d4b5fed732712e7356715b82492e085b46985d7
Sébastien Boulet <sebastien.boulet@gmail.com>
2020-01-18 19:06:36 +0100

    prepare ci integration

--- www/cypress/integration/rchess.spec.js
+++ www/cypress/integration/rchess.spec.js
@@ -1,6 +1,6 @@
 describe('RChess', function() {
     it('is launched properly', function() {
-        cy.visit('http://localhost:8080')
+        cy.visit('/')
 
         cy.get('.board-b72b1').should('be.visible')
         cy.get('.board-b72b1').find('img').should('have.length', 32)
