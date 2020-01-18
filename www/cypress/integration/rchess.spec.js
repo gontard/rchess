41d34d766a00d63acba7967e64c3c7aa7af4a0d1
Sébastien Boulet <sebastien.boulet@gmail.com>
2020-01-18 19:16:10 +0100

    add cypress to github workflow

--- www/cypress/integration/rchess.spec.js
+++ www/cypress/integration/rchess.spec.js
@@ -1,8 +1,10 @@
-describe('RChess', function() {
-    it('is launched properly', function() {
-        cy.visit('/')
+describe("RChess", function() {
+  it("is launched properly", function() {
+    cy.visit("/");
 
-        cy.get('.board-b72b1').should('be.visible')
-        cy.get('.board-b72b1').find('img').should('have.length', 32)
-    })
-})
+    cy.get(".board-b72b1").should("be.visible");
+    cy.get(".board-b72b1")
+      .find("img")
+      .should("have.length", 32);
+  });
+});
